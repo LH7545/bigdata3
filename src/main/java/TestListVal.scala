2ae55c04aeba24cd172e5704cac7f18dd72002d4
root <2518525187@qq.com>
2019-10-22 08:49:44 +0800

    commit First TestListVal

---
+++ src/main/java/TestListVal.scala
@@ -0,0 +1,13 @@
+object TestListVal {
+  def main(array: Array[String]): Unit = {
+      var list : List[Any] = List(
+        "a string",
+        123,
+        'c',
+        true,
+        () => "an anonymous function returning a string"
+      )
+//    list = List(1,2,3)
+    println(list)
+  }
+}
